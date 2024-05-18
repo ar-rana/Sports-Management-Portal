1fc92156d7a7578c0e1b0dc7673f870754fcde96
Aryan Rana <aryan.rana2005@gmail.com>
2024-05-18 14:31:25 +0530

    fixed problem with tournaments page

--- client/src/pages/Tournaments.js
+++ client/src/pages/Tournaments.js
@@ -4,25 +4,25 @@ import React, { useState } from "react";
 const Tournaments = () => {
   const [sport, setSport] = useState("");
 
-  const onOptionChangeHandler = (e) => {
+  const onSubmitHandler = (e) => {
     e.preventDefault();
-    setSport(e.target.value);
-    alert(`sport value set to ${sport}`);
-  };
+    alert(`Sport value set to ${sport}`);
+  }
+
   return (
     <div>
       <nav className="bg-green-600 h-16 w-full flex justify-center place-items-center  ">
         <a href="/" title="Home">
-          <ArrowLeftIcon className="h-8 bg-white rounded-full absolute w-auto m-2 top-2 left-5 hover:bg-gray-300 hover:shadow-lg cursor-pointer"/>
+          <ArrowLeftIcon className="h-8 bg-white rounded-full absolute w-auto m-2 top-2 left-5 hover:bg-gray-300 hover:shadow-lg cursor-pointer" />
         </a>
         <h1 className="text-sky-50 text-3xl font-medium tracking-wide">
           Tournaments
         </h1>
       </nav>
-      <div className="m-4 border-2 p-2">
-        <form className="px-7" onSubmit={onOptionChangeHandler}>
+      <div className="m-4 border-2 p-2 flex items-center">
+        <form className="px-7" onSubmit={onSubmitHandler}>
           <span className="font-bold p-2 text-lg pr-4">Filter a Sport</span>
-          <select name="sports" onChange={(e) => setSport(e.target.value)}>
+          <select name="sports" onChange={(e)=>setSport(e.target.value)} value={sport}>
             <option value="">Select</option>
             <option value="Football">Football</option>
             <option value="Cricket">Cricket</option>
@@ -37,6 +37,9 @@ const Tournaments = () => {
             value="Search"
           />
         </form>
+        <button className="bg-green-600 rounded-3xl w-auto h-auto font-bold text-white hover:shadow-md py-1.5 xl:ml-auto px-4">
+          Register a Tournament
+        </button>
       </div>
       <div className="bg-white h-auto flex flex-col place-content-center justify-center">
         <div className="flex flex-col items-center my-6 space-y-3 w-auto">
@@ -45,9 +48,13 @@ const Tournaments = () => {
               <span className="bg-white m-4 rounded-md font-bold p-2">
                 Name of Tournament
               </span>
-              <span className="bg-white m-4 rounded-md font-bold p-2" >Date</span>
+              <span className="bg-white m-4 rounded-md font-bold p-2">
+                Date
+              </span>
             </div>
-            <span className="xl:ml-auto bg-white m-4 rounded-md font-bold p-2 cursor-pointer">View Details</span>
+            <span className="xl:ml-auto bg-white m-4 rounded-md font-bold p-2 cursor-pointer">
+              View Details
+            </span>
           </div>
         </div>
       </div>
