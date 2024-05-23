66a1fac1a36180b06bb3643e9e1424486c07ee90
Aryan Rana <aryan.rana2005@gmail.com>
2024-05-23 14:37:47 +0530

    configed env

--- server/Index.js
+++ server/Index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const http = require('http');
+require('dotenv').config();
 
 const app = express();
 
 const origin = "http://localhost:3000";
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const options = {
   origin: origin,
