0a17076f36ab32233b8fc53ee20a94b3b99b5aef
Aryan Rana <aryan.rana2005@gmail.com>
2024-05-30 17:00:53 +0530

    added coach card component

---
+++ client/src/components/CoachCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import demo from "../assets/Images/demo.png";
+
+const CoachCard = () => {
+  return (
+    <div>
+      <div className="m-3 p-5 items-center">
+        <div>
+          <img src={demo} className="h-40" />
+        </div>
+        <div className="rounded-2xl bg-green-300 p-3 items-center">
+          <h4>Name & Sport</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CoachCard;
--- client/src/pages/Coaches.js
+++ client/src/pages/Coaches.js
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../assets/Images/logo.png";
-import demo from "../assets/Images/demo.png";
+import CoachCard from "../components/CoachCard";
 
 const Coaches = () => {
   const loop = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
@@ -13,23 +13,14 @@ const Coaches = () => {
           </div>
           <div className="absolute top-5 left-6">
             <a href="/">
-              <img src={logo} className="xl:h-16 md:h-16 h-10 rounded-md" />
+              <img src={logo} className="xl:h-12 md:h-12 h-10 rounded-md" />
             </a>
           </div>
         </nav>
         
         <div className="flex flex-wrap my-10 mx-20 justify-start items-center">
           {loop.map((each, index) => (
-            <>
-              <div key={index} className="m-3 p-5 items-center">
-                <div>
-                  <img src={demo} className="h-40" />
-                </div>
-                <div className="rounded-2xl bg-green-300 p-3 items-center">
-                  <h4>Name & Sport</h4>
-                </div>
-              </div>
-            </>
+            <CoachCard key={index}/>
           ))}
           {/* <div className="flex flex-col">
             <div>
