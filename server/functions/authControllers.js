7903f41c16db74b79cb2d706393f4ed4428f8b24
Aryan Rana <aryan.rana2005@gmail.com>
2024-06-01 16:45:19 +0530

    created login api

--- server/functions/authControllers.js
+++ server/functions/authControllers.js
@@ -1,5 +1,13 @@
 const { db } = require("../firebase");
-const { addDoc, collection, where, query, getDocs } = require("firebase/firestore");
+const {
+  addDoc,
+  collection,
+  where,
+  query,
+  getDocs,
+} = require("firebase/firestore");
+
+//signup
 
 module.exports.signup = async (req, res) => {
   if (req.body.position === "Student") {
@@ -8,7 +16,7 @@ module.exports.signup = async (req, res) => {
     console.log(name, email, rollno, password, position);
 
     try {
-      const q = query(collection(db, "users"), where("email", "==", email ));
+      const q = query(collection(db, "users"), where("email", "==", email));
       const element = await getDocs(q);
       if (element.empty) {
         const docref = await addDoc(collection(db, "users"), {
@@ -24,19 +32,20 @@ module.exports.signup = async (req, res) => {
           name: name,
           email: email,
           rollno: rollno,
-          position: position
-        }
+          position: position,
+        };
         res.status(201).json({
-          user : createdUser,
-          message: "User created successfully"
+          user: createdUser,
+          message: "User created successfully",
         });
       } else {
         res.status(400).json({
-          message: "User already exists"
+          message: "User already exists",
         });
       }
     } catch (e) {
       console.log(e);
+      res.status(500);
     }
   } else {
     const { name, email, password, position } = req.body;
@@ -44,7 +53,7 @@ module.exports.signup = async (req, res) => {
     console.log(name, email, password, position);
 
     try {
-      const q = query(collection(db, "users"), where("email", "==", email ));
+      const q = query(collection(db, "users"), where("email", "==", email));
       const element = await getDocs(q);
       if (element.empty) {
         const docref = await addDoc(collection(db, "users"), {
@@ -58,19 +67,58 @@ module.exports.signup = async (req, res) => {
           id: docref.id,
           name: name,
           email: email,
-          position: position
-        }
+          position: position,
+        };
         res.status(201).json({
-          user : createdUser,
-          message: "User created successfully"
+          user: createdUser,
+          message: "User created successfully",
         });
       } else {
         res.status(400).json({
-          message: "User already exists"
+          message: "User already exists",
         });
       }
     } catch (e) {
       console.log(e);
+      res.status(500);
     }
+  }
+};
+
+//login
+
+module.exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  console.log(email, password);
+  try {
+    const q = query(collection(db, "users"), where("email", "==", email));
+    const user = await getDocs(q);
+    if (!user.empty) {
+      const userDoc = user.docs[0]; //because getDocs() return a "QueryDocumentSnapshot" that needs to be looped through to get data. Here user is the QueryDocumentSnapshot
+      const userData = userDoc.data()
+      if (userData.password === password){
+        const existingUser = {
+          id: userDoc.id,
+          name: userData.name,
+          email: email,
+          position: userData.position,
+        };
+        console.log(existingUser);
+        res.status(200).json({
+          user: existingUser,
+          message: "Login Sucessfull"
+        });
+      } else {
+        res.status(401).json({
+          message: "Incorrect Password"
+        });
+      }
+    } else {
+      res.status(404).json({
+        message: "User Not Found",
+      });
+    }
+  } catch (e) {
+    console.log(e);
   }
 };
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 const authControllers = require("../functions/authControllers");
 const router = Router();
 
-router.post("/signup", authControllers.signup)
+router.post("/signup", authControllers.signup);
+
+router.post("/login", authControllers.login);
 
 module.exports = router;
